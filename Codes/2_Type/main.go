5cd2123abc00ff2f0e987584e9232cafc736b346
barisma <barisma99@gmail.com>
2019-08-12 21:15:19 +0700

    add ans to type code

--- Codes/2_Type/main.go
+++ Codes/2_Type/main.go
@@ -5,20 +5,21 @@ import (
 )
 
 func main() {
+
 	//Number
 	var integer int8
 	integer = -10
 	var unsignedInteger uint8
 	unsignedInteger = 10
-	fmt.Println(integer, unsignedInteger)
+	fmt.Println(integer, unsignedInteger) //-10 10
 
 	var floatingPointNumber float32
 	floatingPointNumber = 10.12
-	fmt.Println(floatingPointNumber)
+	fmt.Println(floatingPointNumber) //10.12
 
 	//String
-	fmt.Println(len("Hello World"))
-	fmt.Println("Hello World"[1])
+	fmt.Println(len("Hello World")) //11
+	fmt.Println("Hello World"[1])   //101
 	fmt.Printf("%c\n", "Hello World"[1])
 	fmt.Println("Hello " + "World")
 
